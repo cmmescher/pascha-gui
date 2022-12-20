6de88ee08cc07139a04ec1e3a50c4b6f50e61048
Christopher Michael Mescher <christopher@mescher.faith>
2022-12-20 16:23:18 -0500

    implement Pascha calculator model

---
+++ src/pascha_calculator_model.cpp
@@ -0,0 +1,96 @@
+#include "pascha_calculator_model.h"
+#include "calendar_conversion.h"
+
+#include <ctime>
+#include <limits>
+
+namespace pascha
+{
+
+void PaschaCalculatorModel::setCalculationMethod(
+    std::unique_ptr<ICalculationMethod> calculation_method)
+{
+  m_calculation_method = std::move(calculation_method);
+}
+
+void PaschaCalculatorModel::calculate(Year year) const
+{
+  try {
+    Date date = m_calculation_method->calculate(year);
+  } catch (const std::overflow_error& e) {
+    notify(e.what());
+  }
+} // PaschaCalculatorModel::calculate
+
+void PaschaCalculatorModel::daysUntil(Year year) const
+{
+  try {
+    CalcInt dateJdn{gregorianToJdn(m_calculation_method->calculate(year))};
+
+    std::time_t t = std::time(0); // get time now
+    std::tm* now = std::localtime(&t);
+    Date nowDate{};
+    nowDate.year = now->tm_year + 1900;
+    nowDate.month = now->tm_mon + 1;
+    nowDate.day = now->tm_mday;
+    CalcInt nowJdn{gregorianToJdn(nowDate)};
+
+    notify(Days{dateJdn - nowJdn});
+  } catch (const std::overflow_error& e) {
+    notify(e.what());
+  }
+} // PaschaCalculatorModel::daysUntil
+
+void PaschaCalculatorModel::weeksBetween(
+    Year year, std::unique_ptr<ICalculationMethod> method1,
+    std::unique_ptr<ICalculationMethod> method2) const
+{
+  try {
+    CalcInt date1Jdn{gregorianToJdn(method1->calculate(year))};
+    CalcInt date2Jdn{gregorianToJdn(method2->calculate(year))};
+
+    notify(Weeks{(date1Jdn - date2Jdn) / 7});
+  } catch (const std::overflow_error& e) {
+    notify(e.what());
+  }
+} // PaschaCalculatorModel::weeksBetween
+
+void PaschaCalculatorModel::addObserver(IObserver& observer)
+{
+  m_observers.push_back(&observer);
+} // PaschaCalculatorModel::addObserver
+
+void PaschaCalculatorModel::removeObserver(IObserver& observer)
+{
+  std::erase(m_observers, &observer);
+} // PaschaCalculatorModel::removeObserver
+
+void PaschaCalculatorModel::notify(const OutputDate& date) const
+{
+  for (IObserver* observer : m_observers) {
+    observer->update(date);
+  }
+} // PaschaCalculatorModel::notify
+
+void PaschaCalculatorModel::notify(Weeks weeks) const
+{
+  for (IObserver* observer : m_observers) {
+    observer->update(weeks);
+  }
+} // PaschaCalculatorModel::notify
+
+void PaschaCalculatorModel::notify(Days days) const
+{
+  for (IObserver* observer : m_observers) {
+    observer->update(days);
+  }
+} // PaschaCalculatorModel::notify
+
+void PaschaCalculatorModel::notify(std::string_view message) const
+{
+  for (IObserver* observer : m_observers) {
+    observer->update(message);
+  }
+} // PaschaCalculatorModel::notify
+
+} // namespace pascha
---
+++ src/pascha_calculator_model.h
@@ -0,0 +1,34 @@
+#ifndef PASCHA_PASCHA_CALCULATOR_MODEL_H
+#define PASCHA_PASCHA_CALCULATOR_MODEL_H
+
+#include "i_calculator_model.h"
+#include <vector>
+namespace pascha
+{
+
+class PaschaCalculatorModel : public ICalculatorModel
+{
+ public:
+  PaschaCalculatorModel() = default;
+  virtual ~PaschaCalculatorModel() = default;
+
+  virtual void
+      setCalculationMethod(std::unique_ptr<ICalculationMethod>) override;
+  virtual void calculate(Year) const override;
+  virtual void daysUntil(Year) const override;
+  virtual void weeksBetween(Year, std::unique_ptr<ICalculationMethod>,
+                            std::unique_ptr<ICalculationMethod>) const override;
+  virtual void addObserver(IObserver&) override;
+  virtual void removeObserver(IObserver&) override;
+  virtual void notify(const OutputDate&) const override;
+  virtual void notify(Weeks) const override;
+  virtual void notify(Days) const override;
+  virtual void notify(std::string_view) const override;
+
+ private:
+  std::unique_ptr<ICalculationMethod> m_calculation_method{nullptr};
+  std::vector<IObserver*> m_observers{};
+}; // class PaschaCalculatorModel
+
+} // namespace pascha
+#endif // !PASCHA_PASCHA_CALCULATOR_MODEL_H
