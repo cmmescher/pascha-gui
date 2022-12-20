360cafeb8bc86ce78b3055f44db51e0838e24656
Christopher Michael Mescher <christopher@mescher.faith>
2022-12-20 16:21:53 -0500

    update observable and observer interface

--- src/i_observer.h
+++ src/i_observer.h
@@ -14,7 +14,7 @@ class IObserver
  public:
   virtual ~IObserver() = default;
   // Used to receive date outputs (e.g. from the model).
-  virtual void update(OutputDate) = 0;
+  virtual void update(const OutputDate&) = 0;
   // Used to receive number of week updates (e.g. from the model).
   virtual void update(Weeks) = 0;
   // Used to receive number of day updates (e.g. from the model).
