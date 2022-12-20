738410644dc0ca742e9bb1473eb03558386b2fdb
Christopher Michael Mescher <christopher@mescher.faith>
2022-12-20 14:23:43 -0500

    implement target date shift function

---
+++ src/target_date.cpp
@@ -0,0 +1,30 @@
+#include "target_date.h"
+
+#include <ctime>
+
+namespace pascha
+{
+
+void TargetDate::shift(std::int64_t days, Date& date) const
+{
+  std::tm t{};
+  t.tm_year = date.year - 1900;
+  t.tm_mon = date.month - 1;
+  t.tm_mday = date.day;
+
+  t.tm_mday += days;
+  std::mktime(&t);
+
+  date.year = t.tm_year + 1900;
+  date.month = t.tm_mon + 1;
+  date.day = t.tm_mday;
+}
+
+Date TargetDate::calculate(Year year) const
+{
+  Date date = m_calculation_method->calculate(year);
+  shift(m_shift_amount, date);
+  return date;
+}
+
+} // namespace pascha
