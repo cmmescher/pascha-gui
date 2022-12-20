d1484c4f10bea903344876919362da4fda7378dc
Christopher Michael Mescher <christopher@mescher.faith>
2022-12-20 14:34:33 -0500

    implement output calendars

---
+++ src/output_calendars.cpp
@@ -0,0 +1,22 @@
+#include "output_calendars.h"
+#include "calendar_conversion.h"
+
+namespace pascha
+{
+
+Date JulianOutputCalendar::calculate(Year year) const
+{
+  return gregorianToJulian(calculation_method().calculate(year));
+} // JulianOutputCalendar::calculate
+
+Date GregorianOutputCalendar::calculate(Year year) const
+{
+  return calculation_method().calculate(year);
+} // GregorianOutputCalendar::calculate
+
+Date RevisedJulianOutputCalendar::calculate(Year year) const
+{
+  return gregorianToRevJulian(calculation_method().calculate(year));
+} // JulianOutputCalendar::calculate
+
+} // namespace pascha
